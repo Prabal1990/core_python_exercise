86560588697b7a588fa795cfc0688292c26af60d
Prabal <prabal199107@gmail.com>
2023-02-16 09:38:53 -0500

    Added in List

--- List.py
+++ List.py
@@ -16,3 +16,14 @@ print(motorcycles)
 motorcycles[0] = 'ducati'
 print(motorcycles)
 
+# LOOPING
+magicians = ['alice', 'david', 'carolina'] 
+# output- Alice, that was a great trick! 
+# output- I can't wait to see your next trick, Alice. 
+for i in magicians:
+    # print(i, "that was a great trick!")
+    print(f"{i.title()},that was a great trick! ")
+    print(f"I can't wait to see your next trick, {i.title()}.\n")
+
+# print(f"Hello, {fullname.title()}!")
+
